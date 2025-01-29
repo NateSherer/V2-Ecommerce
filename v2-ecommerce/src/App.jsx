3d2e8ebbc087c0900ddcdd547ba8f6d9f72d156b
Nathaniel L. Sherer <NateSherer@Administrators-MacBook-Pro.local>
2025-01-29 02:04:25 -0500

    change-navbar

--- v2-ecommerce/src/App.jsx
+++ v2-ecommerce/src/App.jsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from './Pages/Homepage'; 
-import Form from './Pages/Contact';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import Products from './Pages/Products'
-
-
-
-
-
+import Homepage from "./Pages/Homepage";
+import Form from "./Pages/Contact";
+import Footer from "./components/Footer";
+import Products from "./Pages/Products";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/form" element={<Form />} />
-        <Route path="/products" element={<Products />} /> 
+        <Route path="/products" element={<Products />} />
       </Routes>
       <Footer />
     </BrowserRouter>
